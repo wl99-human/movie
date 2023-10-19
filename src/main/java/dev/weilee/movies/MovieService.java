7364a6bffda336e404455ba9e75bba4c9a692261
wl99-human <weileelim2012@gmail.com>
2023-10-20 00:09:35 +0800

    Done movie endpoints

--- src/main/java/dev/weilee/movies/MovieService.java
+++ src/main/java/dev/weilee/movies/MovieService.java
@@ -1,9 +1,11 @@
 package dev.weilee.movies;
 
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class MovieService {
@@ -13,5 +15,9 @@ public class MovieService {
 
     public List<Movie> findAllMovies() {
         return movieRepository.findAll();
+    }
+
+    public Optional<Movie> findMovieById(ObjectId id) {
+        return movieRepository.findById(id);
     }
 }
