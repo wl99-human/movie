a366f8a9b17c08139f4d3b4f1367b7b52a6bd314
wl99-human <weileelim2012@gmail.com>
2023-10-20 00:15:22 +0800

    Added CORS configuration

--- src/main/java/dev/weilee/movies/MoviesApplication.java
+++ src/main/java/dev/weilee/movies/MoviesApplication.java
@@ -2,7 +2,10 @@ package dev.weilee.movies;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @RestController
@@ -10,5 +13,19 @@ public class MoviesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MoviesApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("*")
+						.allowedMethods("*")
+						.allowedHeaders("*")
+						.allowCredentials(false).maxAge(3600);
+			}
+		};
 	}
 }
