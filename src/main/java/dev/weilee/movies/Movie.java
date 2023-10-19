08731612dbd96339a2f5d97552a8f37f2c9d4da7
wl99-human <weileelim2012@gmail.com>
2023-10-20 00:25:45 +0800

    Added title in movie

--- src/main/java/dev/weilee/movies/Movie.java
+++ src/main/java/dev/weilee/movies/Movie.java
@@ -8,7 +8,6 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.DocumentReference;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 @Document(collection = "movies")
@@ -30,6 +29,7 @@ public class Movie {
 
     public Movie(String imdbId, String title, String releaseDate, String trailerLink, String poster, List<String> backdrops, List<String> genres) {
         this.imdbId = imdbId;
+        this.title = title;
         this.releaseDate = releaseDate;
         this.trailerLink = trailerLink;
         this.poster = poster;
