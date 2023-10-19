f7cef94a2eb946f52d13df0833b1e1aec4b9b865
wl99-human <weileelim2012@gmail.com>
2023-10-20 00:08:01 +0800

    Added project lombok and implemented REST function

---
+++ src/main/java/dev/weilee/movies/MovieController.java
@@ -0,0 +1,21 @@
+package dev.weilee.movies;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/movies")
+public class MovieController {
+
+    @Autowired
+    private MovieService movieService;
+
+    @GetMapping
+    public ResponseEntity getMovies() {
+        return ResponseHandler.generateResponse(movieService.findAllMovies(), HttpStatus.OK);
+    }
+}
